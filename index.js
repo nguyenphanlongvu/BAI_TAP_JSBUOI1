8ca4a934476151353d962abfe83c3ff0a1b45604
nguyenphanlongvu <vuvt6019@gmail.com>
2023-08-08 18:56:01 +0700

    update file index.js

---
+++ index.js
@@ -0,0 +1,75 @@
+// Bài 1
+var luong1Ngay = 100000;
+var soNgaylam = 30 ;
+var luongThang = luong1Ngay * soNgaylam ;
+console.log("😃 - luongThang:", luongThang)
+
+
+
+// bai 2
+var giaTri1 = 24;
+var giaTri2 = 30;
+var giaTri3 = 32;
+var giaTri4 = 46;
+var giaTri5 = 25;
+var trungBinhcong = (giaTri1 + giaTri2 + giaTri3 +giaTri4) /5
+console.log("😃 - trungBinhcong:", trungBinhcong)
+
+//bai 3
+var giaUsdhiennay = 23000 ;
+var soUsd = 2;
+var soTien = giaUsdhiennay * soUsd ;
+console.log("😃 - soTien:", soTien)
+
+//bai 4
+var chieuDai = 10;
+var chieuRong = 5 ;
+var chuVi = (chieuDai + chieuRong)*2;
+console.log("😃 - chuVi:", chuVi)
+var dienTich = chieuDai * chieuRong
+console.log("😃 - dienTich:", dienTich)
+
+
+
+
+
+// bai5
+var nhapSo1 = 12;
+var hangChuc = Math.floor(nhapSo1 / 10)
+var hangDonvi = nhapSo1 % 10
+var tongChuso1= hangChuc + hangDonvi
+console.log("😃 - tongChuso1:", tongChuso1);
+
+
+var nhapSo2 = 44;
+var hangChuc = Math.floor(nhapSo2 / 10)
+var hangDonvi = nhapSo2 % 10
+var tongChuso2= hangChuc + hangDonvi
+console.log("😃 - tongChuso2:", tongChuso2);
+
+
+var nhapSo3 = 83;
+var hangChuc = Math.floor(nhapSo3 / 10)
+var hangDonvi = nhapSo3 % 10
+var tongChuso3= hangChuc + hangDonvi
+console.log("😃 - tongChuso3:", tongChuso3);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
